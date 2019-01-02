1e9516eb87ce562c5ea79faf1edb40c9d5d1f5a2
Florian Diebold <flodiebold@googlemail.com>
2019-01-02 07:59:21 +0000

    Add a command line option to pre-select a certain child node by key (#10)
    E.g. I can bind $mod+space to `spacerun`, and $mod+l to `spacerun -k l` to
    directly go to the 'l' subtree.
    
    This uses structopt to do the argument parsing; another possibility would be
    using clap directly, but I like structopt ;)
    
    In the course of this, simplify / refactor shortcut parsing: Remove unused
    Deserialize implementation for KeyCode, implement FromStr for both, and
    implement Deserialize in a simpler way.

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,6 @@
 use conrod::glium::glutin::dpi::{LogicalPosition, LogicalSize};
 
+use crate::Options;
 use crate::commands::Command;
 use crate::config::SpacerunConfig;
 
@@ -11,16 +12,30 @@ pub struct State {
     pub window_dimensions: LogicalSize,
     pub config: SpacerunConfig,
     pub selected_command: Command,
+    pub options: Options,
 }
 
 impl State {
-    pub fn new(config: SpacerunConfig) -> State {
+    pub fn new(config: SpacerunConfig, options: Options) -> State {
         let state = State {
             window_dimensions: (DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT).into(),
             window_position: (0, 0).into(),
-            selected_command: config.commands.to_owned(),
-            config: config,
+            selected_command: select_initial_command(&config, &options),
+            config,
+            options,
         };
         return state;
     }
+}
+
+fn select_initial_command(config: &SpacerunConfig, options: &Options) -> Command {
+    let mut command = &config.commands;
+
+    if let Some(shortcut) = &options.initial_shortcut {
+        if let Some(child) = command.find_child_for_shortcut(shortcut) {
+            command = child;
+        }
+    };
+
+    command.to_owned()
 }
